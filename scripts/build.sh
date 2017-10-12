bdd2a5e1251b8e15c09edb46a163cb6c70763915
Mevan Samaratunga <mevansam@gmail.com>
2017-10-12 11:11:03 +0200

    adding travis build scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,59 +7,56 @@ if [[ -z "$TRAVIS_TAG" ]] ; then
     exit $?
 fi
 
-function pcfdev_instance_detail() {
-    aws ec2 describe-instances \
-        | jq '.Reservations[] | .Instances[] | select(.State.Name != "terminated") | select(.Tags[] | .Value == "pcfdev")'
-}
+# function pcfdev_instance_detail() {
+#     aws ec2 describe-instances \
+#         | jq '.Reservations[] | .Instances[] | select(.State.Name != "terminated") | select(.Tags[] | .Value == "pcfdev")'
+# }
 
-set -e
+# set -e
 
-PCFDEV_INSTANCE_DETAIL=$(pcfdev_instance_detail)
-if [[ -z $PCFDEV_INSTANCE_DETAIL ]]; then
-    echo "ERROR! Unable to discover PCFDev instance."
-    exit 1
-fi
+# PCFDEV_INSTANCE_DETAIL=$(pcfdev_instance_detail)
+# if [[ -z $PCFDEV_INSTANCE_DETAIL ]]; then
+#     echo "ERROR! Unable to discover PCFDev instance."
+#     exit 1
+# fi
 
-scripts/pcfdev-access.sh
+# scripts/pcfdev-access.sh
 
-PUBLIC_IP=$(echo $PCFDEV_INSTANCE_DETAIL | jq -r .PublicIpAddress)
-PRIVATE_IP=$(echo $PCFDEV_INSTANCE_DETAIL | jq -r .PrivateIpAddress)
+# PUBLIC_IP=$(echo $PCFDEV_INSTANCE_DETAIL | jq -r .PublicIpAddress)
+# PRIVATE_IP=$(echo $PCFDEV_INSTANCE_DETAIL | jq -r .PrivateIpAddress)
 
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i .test_env/pcfdev.pem ubuntu@$PUBLIC_IP <<EOF
-#!/bin/bash
+# ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i .test_env/pcfdev.pem ubuntu@$PUBLIC_IP <<EOF
+# #!/bin/bash
 
-rm -fr /tmp/gopath
-mkdir -p /tmp/gopath/src/github.com/terraform-providers/terraform-provider-ldap
-EOF
+# rm -fr /tmp/gopath
+# mkdir -p /tmp/gopath/src/github.com/terraform-providers/terraform-provider-ldap
+# EOF
 
-scp -q -r -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i .test_env/pcfdev.pem \
-    ./* ubuntu@$PUBLIC_IP:/tmp/gopath/src/github.com/terraform-providers/terraform-provider-ldap
+# scp -q -r -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i .test_env/pcfdev.pem \
+#     ./* ubuntu@$PUBLIC_IP:/tmp/gopath/src/github.com/terraform-providers/terraform-provider-ldap
 
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i .test_env/pcfdev.pem ubuntu@$PUBLIC_IP <<EOF
-#!/bin/bash
+# ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i .test_env/pcfdev.pem ubuntu@$PUBLIC_IP <<EOF
+# #!/bin/bash
 
-go version | grep go1.8 >/dev/null 2>&1
-[[ \$? -eq 0 ]] || \
-    sudo rm -fr /usr/local/go/ && curl https://storage.googleapis.com/golang/go1.8.3.linux-amd64.tar.gz | sudo tar xz -C /usr/local
+# go version | grep go1.8 >/dev/null 2>&1
+# [[ \$? -eq 0 ]] || \
+#     sudo rm -fr /usr/local/go/ && curl https://storage.googleapis.com/golang/go1.8.3.linux-amd64.tar.gz | sudo tar xz -C /usr/local
 
-set -ex
+# set -ex
 
-export GOROOT=/usr/local/go
-export GOPATH=/tmp/gopath
-cd /tmp/gopath/src/github.com/terraform-providers/terraform-provider-ldap
+# export GOROOT=/usr/local/go
+# export GOPATH=/tmp/gopath
+# cd /tmp/gopath/src/github.com/terraform-providers/terraform-provider-ldap
 
-export CF_USER=admin
-export CF_PASSWORD=admin
-export CF_SKIP_SSL_VALIDATION=true
-export CF_UAA_CLIENT_ID=admin
-export CF_API_URL=https://api.$PRIVATE_IP.xip.io
-export CF_UAA_CLIENT_SECRET=admin-client-secret
+# export LDAP_HOST=172.16.45.144
+# export LDAP_BIND_DN=cn=admin,dc=example,dc=org
+# export LDAP_BIND_PASSWORD=admin
 
-make testacc
+# make testacc
 
-EOF
+# EOF
 
-set +e
+# set +e
 
 make release
 exit $?
